5aefebee9c0e8a45ee180041d443e2f190e946d8
Kowalsky98 <168223869+Kowalsky98@users.noreply.github.com>
2025-01-30 14:48:43 -0400

    Payroll-Update

---
+++ models/hr_employee_dependent.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+class HrEmployeeDependent(models.Model):
+    _name = "hr.employee.dependent"
+    _description = "Employee Dependent"
+
+    name = fields.Char(string="Nombre del Hijo", required=True)
+    birth_date = fields.Date(string="Fecha de Nacimiento")
+    relationship = fields.Selection([
+        ('son', 'Hijo'),
+        ('daughter', 'Hija'),
+        ('other', 'Otros'),
+    ], string="Relación", required=True)
+    employee_id = fields.Many2one("hr.employee", string="Empleado", ondelete="cascade", required=True)
